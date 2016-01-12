a6416b05985a08d983083e565e50592493ff3669
julien <julien@julien>
2016-01-12 16:20:52 +0100

    api get tout les users ok

--- src/AdminBundle/Controller/ArtisteRestController.php
+++ src/AdminBundle/Controller/ArtisteRestController.php
@@ -3,24 +3,34 @@
 namespace AdminBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use PublicBundle\Entity\Artiste;
+use PublicBundle\Entity\ArtisteRepository;
 
 /**
  * Description of ArtisteRestController
  *
  * @author julien
  */
-class ArtisteRestController extends Controller{
+class ArtisteRestController extends Controller {
 
-    public function getUserAction($username) {
+    /**
+     * 
+     * @return array
+     * @View()
+     * 
+     */
+    public function getArtistesAction() {
+
+//    $user = $this->getDoctrine()->getRepository('AdminBundle:User')->findOneByUsername($username);
         
-    $user = $this->getDoctrine()->getRepository('AdminBundle:User')->findOneByUsername($username);
+          $user = $this->getDoctrine()->getRepository('AdminBundle:User')->findAll();
+          return array("users" => $user);
 
-        if (!is_object($user)) {
-            throw $this->createNotFoundException();
-        }
-        return $user;
+         
+
     }
 
 }
